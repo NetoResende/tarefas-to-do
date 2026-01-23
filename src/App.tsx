c15bd03cf473227d9c05e666b68882294aa29d5c
netoresende <raimundogomes826@gmail.com>
2026-01-23 18:55:04 -0300

    Importando e usando arquivos de css global

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import "./styles/Theme.css"
+import "./styles/Global.css"
+
 export function App() {
   return (
     <>
      <h1>Olá Mundo! ( Este é meu App )</h1>
+     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit quas labore repudiandae facilis deserunt, pariatur dolor, laborum delectus tempora, voluptas molestias blanditiis aliquid fugit rem recusandae neque assumenda. Modi, repudiandae?</p>
     </>
   )
 }
